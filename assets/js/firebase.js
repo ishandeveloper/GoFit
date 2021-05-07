29b4ab70c1c2a3761177c9e3442437190e41330a
ishandeveloper <ishandeveloper@outlook.com>
2021-05-07 23:26:06 +0530

    Initial Commit

---
+++ assets/js/firebase.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyC9qt5ROSIjVu8wLw57vrom6eqeN9d081o",
+  authDomain: "gofit-33fb5.firebaseapp.com",
+  projectId: "gofit-33fb5",
+  storageBucket: "gofit-33fb5.appspot.com",
+  messagingSenderId: "1004440584467",
+  appId: "1:1004440584467:web:4c7cc8c9ea898167ebdae6",
+};
+// Initialize Firebase
+
+firebase.initializeApp(firebaseConfig);
